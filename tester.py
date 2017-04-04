2ad80d1311a68b0e45d5cc6753b66505d284aae8
ankitaraj27 <ankitaraj27@users.noreply.github.com>
2017-04-04 13:33:33 +0530

    Update tester.py

--- tester.py
+++ tester.py
@@ -9,7 +9,7 @@ from sklearn.cross_validation import ShuffleSplit
 from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.metrics import confusion_matrix
 from sklearn.externals import joblib
-
+from classifier_new import length
 from utils import plot_roc_curves, plot_confusion_matrix, GENRE_DIR, GENRE_LIST, TEST_DIR
 
 from ceps import read_ceps, create_ceps_test, read_ceps_test
@@ -24,19 +24,29 @@ clf = None
 #          Please run the classifier script first
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 def test_model_on_single_file(file_path):
-    clf = joblib.load("C:\Users\RAJ\Desktop\matlab\genreXpose-master\genreXpose\saved_model\model_ceps.pkl") #change link
+    clf = joblib.load("C:\Users\RAJ\Desktop\matlab\genreXpose-master\genreXpose\saved_model\model_ceps.pkl")
     #clf1 = joblib.load("C:\Users\RAJ\Desktop\matlab\genreXpose-master\genreXpose\saved_model\model_ceps1.pkl")
     link = create_ceps_test(file_path)
-    X, y = read_ceps_test(link+".npy")
-    #print y
-    probs = clf.predict_proba(X)
+    X_old, y = read_ceps_test(link+".npy")
+    X_old = X_old[0]
+    #probs = clf.decision_function(X)
+    l = length()
+    print l
+    if l>len(X_old):
+        zero = np.zeros(l-len(X_old))
+        X=np.concatenate([np.array(X_old),np.array(zero)])
+    else :
+        X=X_old
+    
     value = clf.predict(X)
-##    #value1 = clf1.predict(X)
+    probs = clf.predict_proba(X)
+    print probs
+    #value1 = clf1.predict(X)
     print "\t".join(str(x) for x in traverse)
     print "\t".join(str("%.3f" % x) for x in (probs[0]))
     probs=probs[0]
     #probs=1-probs
-    max_prob = min(probs)
+    max_prob = max(probs)
     #print max_prob
     for i,j in enumerate(probs):
         if probs[i] == max_prob:
@@ -46,8 +56,8 @@ def test_model_on_single_file(file_path):
     predicted_genre = traverse[max_prob_index]
     pg = traverse[value]
 
-    print "predicted genre by min = ",predicted_genre," by predict = ",pg
-    return pg
+    print "predicted genre = ", pg, " by predict by min= ",predicted_genre
+    return predicted_genre
 
 if __name__ == "__main__":
 
@@ -56,7 +66,7 @@ if __name__ == "__main__":
         traverse = list(set(dirs).intersection(set(GENRE_LIST)))
         break
     fi=[]
-    test_file = "C:/Users/RAJ/Desktop/matlab/test_data"  ##change link
+    test_file = "C:/Users/RAJ/Desktop/matlab/test_data"
     for f in os.listdir(test_file):
         path=test_file+'/'+f
         print "\n\nfor file "+f
